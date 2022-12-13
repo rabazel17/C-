e0716bbf13122c9df3c194e4872a6d5356589b94
Natasha <info@rabzel17.ru>
2022-12-13 20:01:48 +0300

    Решение задачи

---
+++ HW4/task27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число ");
+// int number = ReadInt("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"сумма цифр {sum}");
+}
+
+int ReadLine(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
